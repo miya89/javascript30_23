c6abff68245f89e6390d31356614696c4868b896
MiyaPeynova <maria.peynova@hotmail.com>
2021-08-22 12:03:29 +0100

    Add dropdown functionality to change speaker

--- src/script.js
+++ src/script.js
@@ -4,3 +4,21 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(
+      (voice) =>
+        `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
+    )
+    .join("");
+}
+
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name == this.value);
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
