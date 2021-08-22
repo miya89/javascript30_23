efd6b913cf5458a5f376759036600d32e19f993a
MiyaPeynova <maria.peynova@hotmail.com>
2021-08-22 12:20:54 +0100

    Add func on stop/speak buttons and filtered the en speakers

--- src/script.js
+++ src/script.js
@@ -9,6 +9,8 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
   voices = this.getVoices();
   voicesDropdown.innerHTML = voices
+    // .filter((voice) => voice.lang.includes("en")) - filter out only the english speaking ppl
+
     .map(
       (voice) =>
         `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
@@ -18,7 +20,23 @@ function populateVoices() {
 
 function setVoice() {
   msg.voice = voices.find((voice) => voice.name == this.value);
+  toggle();
 }
 
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
 speechSynthesis.addEventListener("voiceschanged", populateVoices);
 voicesDropdown.addEventListener("change", setVoice);
+options.forEach((option) => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
